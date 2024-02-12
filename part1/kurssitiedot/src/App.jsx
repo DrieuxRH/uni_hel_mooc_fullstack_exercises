45a815d51fda5941ed230b024ae09abe0f85dc76
Reija <reijamehtio@me.com>
2024-02-12 17:26:43 +0100

    Added 1.2

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -7,11 +7,22 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  console.log(props)
+  return(
+    <>
+      <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
 const Content = (props) => {
   console.log(props)
   return (
     <>
-     <p>{props.part} {props.exercises}</p>
+      <Part part = {props.part1} exercises = {props.exercises1} />
+      <Part part = {props.part2} exercises = {props.exercises2} />
+      <Part part = {props.part3} exercises = {props.exercises3} />
     </>
   )
 }
@@ -37,9 +48,9 @@ const App = () => {
   return (
     <div>
       <Header course = {course}/>
-      <Content part = {part1} exercises = {exercises1} />
-      <Content part = {part2} exercises = {exercises2} />
-      <Content part = {part3} exercises = {exercises3} />
+      <Content part1 = {part1} exercises1 = {exercises1} 
+                part2 = {part2} exercises2 = {exercises2}
+                part3 = {part3} exercises3 = {exercises3} />
       <Total ex1 = {exercises1} ex2 = {exercises2} ex3 = {exercises3} />
     
       
