c21e0972b665f7c152f33c1da89030d7756a5de1
Reija <reijamehtio@me.com>
2024-02-18 19:53:23 +0100

    2.1

---
+++ part2/kurssitiedot/src/App.jsx
@@ -0,0 +1,71 @@
+const Course = ({course}) => {
+  return (
+    <>
+      <Header name = {course.name} />
+      <Content parts = {course.parts} />
+    </>
+  )
+}
+
+const Header = ({name}) => <h1>{name}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({parts}) => {
+  return(
+    parts.map(part => 
+      <Part part={part} key={part.id}/>
+    )
+  )
+}
+
+/* Part rendering, every part added manually
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts[0]} 
+    />
+    <Part
+      part={parts[1]} 
+    />
+    <Part
+      part={parts[2]} 
+    />      
+  </>
+*/
+  const App = () => {
+    const course = {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Course course={course} />
+      </div>
+    )
+  }
+  
+export default App
